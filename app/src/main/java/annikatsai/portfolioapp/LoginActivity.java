4eb737267dcdda0771a662e3fd03080274f1c013
Annika Tsai <annikatsai@fb.com>
2016-07-08 10:29:19 -0700

    fb login success

--- app/src/main/java/annikatsai/portfolioapp/LoginActivity.java
+++ app/src/main/java/annikatsai/portfolioapp/LoginActivity.java
@@ -1,9 +1,10 @@
 package annikatsai.portfolioapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -11,47 +12,53 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
+    LoginButton loginButton;
     CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
 
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        Intent i = new Intent(LoginActivity.this, TimelineActivity.class);
-                        startActivity(i);
-                    }
+        // Create callback to handle results of login and register callbackManager
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // Launch TimelineActivity
+                Intent i = new Intent(LoginActivity.this, TimelineActivity.class);
+                startActivity(i);
+            }
 
-                    @Override
-                    public void onCancel() {
-                        // App code
-                    }
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "Login attempt canceled", Toast.LENGTH_SHORT);
+            }
 
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                    }
-                });
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(LoginActivity.this, "Login attempt failed", Toast.LENGTH_SHORT);
+            }
+        });
     }
 
+    // Handles return from login
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+    // onClick method from login button
     public void onClick(View view) {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,4 +67,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
