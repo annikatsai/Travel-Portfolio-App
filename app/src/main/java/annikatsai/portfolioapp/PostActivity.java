424811136435b5b02881ac222f1125b2ef560f26
Cynthia Aguillon <caguillon@fb.com>
2016-07-11 17:44:38 -0700

    Code so far on camera

--- app/src/main/java/annikatsai/portfolioapp/PostActivity.java
+++ app/src/main/java/annikatsai/portfolioapp/PostActivity.java
@@ -1,13 +1,21 @@
 package annikatsai.portfolioapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class PostActivity extends AppCompatActivity {
+// extends AppCompatActivity
+public class PostActivity extends TakePicActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+    }
+
+    public void onAddClick(View view){
+        // Launch TakePic
+        Intent i = new Intent(this, TakePicActivity.class);
+        startActivity(i);
     }
 }
