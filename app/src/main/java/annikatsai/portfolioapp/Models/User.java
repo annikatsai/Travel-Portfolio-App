bd0df17bb37fe28fc6486aa518f2a516b45b0040
Annika Tsai <annikatsai@fb.com>
2016-07-29 09:42:40 -0700

    UI

--- app/src/main/java/annikatsai/portfolioapp/Models/User.java
+++ app/src/main/java/annikatsai/portfolioapp/Models/User.java
@@ -10,6 +10,7 @@ public class User {
     public String email;
     public String name;
     public String coverPhotoUrl;
+    public String link;
 
     public User () {}
 
@@ -17,11 +18,14 @@ public class User {
         this.id = id;
         this.email = email;
         this.name = name;
+        this.link = "";
     }
 
     public String getEmail() {
         return email;
     }
+
+    public String getLink() { return link; }
 
     public String getId() {
         return id;
@@ -46,6 +50,7 @@ public class User {
             u.id = json.getString("id");
             u.name = json.getString("name");
             u.email = json.getString("email");
+            u.link = json.getString("link");
         } catch (JSONException e) {
             e.printStackTrace();
             throw new RuntimeException();
