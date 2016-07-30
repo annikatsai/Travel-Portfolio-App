bfbad2e3702c47bf675880e6e06167659fc0c43d
Annika Tsai <annikatsai@fb.com>
2016-07-30 16:33:43 -0700

    UI, Toolbar

--- app/src/main/java/annikatsai/portfolioapp/TimelineActivity.java
+++ app/src/main/java/annikatsai/portfolioapp/TimelineActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -23,7 +22,6 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
@@ -86,13 +84,13 @@ public class TimelineActivity extends AppCompatActivity implements PostsArrayAda
 
         // Customizing toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        TextView toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
+        //TextView toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         toolbar.setTitle("");
-        Typeface titleFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
-        toolbarTitle.setText("Roam");
-        toolbarTitle.setTypeface(titleFont);
+//        Typeface titleFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+//        toolbarTitle.setText("Roam");
+//        toolbarTitle.setTypeface(titleFont);
 
         mDataBaseReference = FirebaseDatabase.getInstance().getReference();
 
